63f5eb11b37bd92116c7a3d8ca4a0bda688886c9
simaalexandru <alexandrusima04@gmail.com>
2018-02-24 11:32:30 +0100

    Data Structures in ES6
    Using The set, storing multiple types(int, string) and object in the
    same set

--- projectreact/src/App.js
+++ projectreact/src/App.js
@@ -2,24 +2,13 @@ import React  from 'react';
 import './App.css';
 import Animal from './Animal';
 
+let a = new Set();
+//add values or even objects
+//using the add()
 
-function Wizard(name, house, pet){
-  this.name = name;
-  this.house = house;
-  this.pet = pet;
+a.add(5);
+a.add("wohoo");
+a.add({x: 50, y: 200});
 
-  this.greet = () =>  `I'm ${this.name} from ${this.house}`
-}
-
-Wizard.prototype.pet_name; //function
-//arrow function doesn't create a this object for the function prototype to referenc
-//instead, when use the prototype keyword to define a method , use normal function declaration
-Wizard.prototype.info = function(){
-  return `I have ${this.pet} named ${this.pet_name}`
-};
-
-let harry = new Wizard("Harry Potter", "Gryffindoor" , "Owl");
-harry.pet_name = "Hedwig";
-
-console.log(harry.info());
+console.log(a);
 export default 'App';
