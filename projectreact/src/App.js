728ac77956de7ec1760d1d03e4663a57f9de0401
simaalexandru <alexandrusima04@gmail.com>
2018-02-24 11:56:57 +0100

    Map - Data Set
    keys of a map can be string, object or function

--- projectreact/src/App.js
+++ projectreact/src/App.js
@@ -2,9 +2,14 @@ import React  from 'react';
 import './App.css';
 import Animal from './Animal';
 
-let chars = 'dsakdnajsvdenwidewdjwdewdewedw';
-let charArr = chars.split("");
-let charSet = new Set(charArr);
-console.log(charSet);
+let a = new Map();
+let key_1 = "string key";
+let key_2 = {a: 'key '};
+let key_3 = function(){};
+a.set(key_1, 'return value for a string key');
+a.set(key_2, 'return value for an object key');
+a.set(key_3, 'return value for a function key');
+
+console.log(a);
 
 export default 'App';
