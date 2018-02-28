3247b298a012a2696ff339553f4b5533328db3cc
simaalexandru <alexandrusima04@gmail.com>
2018-02-28 13:26:33 +0100

    Passing state to a generator
    Using reset, to reset the counter

--- projectreact/src/App.js
+++ projectreact/src/App.js
@@ -5,15 +5,24 @@ import Animal from './Animal';
 //keeping track of your customers
 //each time someone walks on the door, the generator will count
 
-function* countMaker() {
+function* evens() {
   let count = 0;
-  while(count < 3){
-    yield count += 1;
+  while(true){
+    count += 2; //endless loop
+    let reset = yield count;
+    if (reset){ //if reset is true
+      count = 0;
+    }
   }
 }
+//whatever we pass as a parameter to next()
+//can modify the internal state and behavior of the generator
 
-let countGen = countMaker();
-console.log(countGen.next().value); //1
-console.log(countGen.next().value); //2
-console.log(countGen.next().value); //3
-console.log(countGen.next().value); //undefined
+//set the yield count to a reset variable
+//the reset looks for what is passed to the generator and checks for it in a yield statement
+let sequence = evens();
+console.log(sequence.next().value);
+console.log(sequence.next().value);
+console.log(sequence.next().value);
+console.log(sequence.next(true).value);
+console.log(sequence.next().value);
